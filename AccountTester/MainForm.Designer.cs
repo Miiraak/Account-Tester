f4ea9f196e342fa09e41feaf4905d87c005591f1
Miiraak <miiraak@feynlok.ch>
2025-02-17 23:04:47 +0100

    Update title bar text and improve test count accuracy
    - Updated title bar text in `MainForm` to "Account Tester v0.7".
    - Moved increment of `ExportVariables.General_export_TotalTests` inside the loop in `NetworkStorageRightsTesting` to ensure accurate test counts.
    - Removed redundant increment of `ExportVariables.General_export_TotalSuccess`.
    - Cleaned up code by removing an empty line in the printer iteration loop.
    - Made minor formatting adjustments for better readability.

--- AccountTester/MainForm.Designer.cs
+++ AccountTester/MainForm.Designer.cs
@@ -103,7 +103,7 @@
             Icon = (Icon)resources.GetObject("$this.Icon");
             MaximizeBox = false;
             Name = "MainForm";
-            Text = "Account Tester v0.6.2";
+            Text = "Account Tester v0.7";
             ResumeLayout(false);
             PerformLayout();
         }
